d08fa550f2acab0825e3dc56ca989c43c472bf5d
Pedro Souza <pedrosouza@MacBook-Air-de-Pedro.local>
2024-05-14 00:56:30 -0300

    adding casl configs

---
+++ apps/api/src/index.ts
@@ -0,0 +1,5 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+console.log(userCanDeleteOtherUsers)
