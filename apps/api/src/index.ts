80af40f1351dd7c1e089286aaabc10f7f533785d
Pedro Souza <pedrosouza@MacBook-Air-de-Pedro.local>
2024-05-14 14:35:50 -0300

    adding all permisions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,8 @@
 import { defineAbilityFor } from '@saas/auth'
+import { projectSchema } from '@saas/auth/src/models/project'
 
-const ability = defineAbilityFor({ role: 'ADMIN' })
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-console.log(userCanInviteSomeoneElse)
+const project = projectSchema.parse({ id: 'projectId', ownerId: 'user-id' })
+
+console.log(ability.can('delete', project))
