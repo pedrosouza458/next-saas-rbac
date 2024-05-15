7343db42a17a304ffe961c3fda6b84ec278bf675
Pedro Souza <pedrosouza@MacBook-Air-de-Pedro.local>
2024-05-15 02:59:40 -0300

    adding fastify

--- apps/api/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-import { projectSchema } from '@saas/auth/src/models/project'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const project = projectSchema.parse({ id: 'projectId', ownerId: 'user-id' })
-
-console.log(ability.can('delete', project))
