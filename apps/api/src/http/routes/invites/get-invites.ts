e0af50a8168b566bf2c03e7d078c617b1b4096b8
Pedro Souza <pedrosouza@MacBook-Air-de-Pedro.local>
2024-05-29 14:02:48 -0300

    last fixes

---
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -0,0 +1,86 @@
+import { roleSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Get all organization invites',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string(),
+                  email: z.string().email(),
+                  role: roleSchema,
+                  createdAt: z.date(),
+                  author: z
+                    .object({
+                      name: z.string().nullable(),
+                      id: z.string().uuid(),
+                    })
+                    .nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedError(
+            `You're not allowed to get organizations invite.`,
+          )
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            organizationId: organization.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'asc',
+          },
+        })
+
+        return { invites }
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,14 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { getOrganizationBilling } from './routes/billing/get-organization-billing'
+import { acceptInvite } from './routes/invites/accept-invite'
+import { createInvite } from './routes/invites/create-invite'
+import { deleteInvite } from './routes/invites/delete-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
+import { getPendingInvites } from './routes/invites/get-pending-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
 import { deleteMember } from './routes/members/delete-member'
 import { getMembers } from './routes/members/get-members'
 import { updateMember } from './routes/members/update-members'
@@ -95,6 +103,7 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
+app.register(getPendingInvites)
 
 /**
  * Members
@@ -102,6 +111,21 @@ app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
 app.register(deleteMember)
+
+/**
+ * Invites
+ */
+app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(deleteInvite)
+
+/**
+ * Billing
+ */
+app.register(getOrganizationBilling)
 
 app.register(fastifySwaggerUi, {
   routePrefix: '/docs',
